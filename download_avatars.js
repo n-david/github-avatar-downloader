5978760d6031f92df3164d61961ede932768bbad
n-david <ngdavid.93@gmail.com>
2017-02-13 22:25:51 +0000

    Added requestURL for download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,12 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+var GITHUB_USER = 'n-david';
+var GITHUB_TOKEN = 'd26c217e18600e157b3df53b93e823177d6aff4d';
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
