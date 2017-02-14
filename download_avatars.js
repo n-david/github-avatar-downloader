9a6129e513d84518c580d6bccc1d7b021a0bf708
n-david <ngdavid.93@gmail.com>
2017-02-14 00:31:12 +0000

    Edited download_avatars.js to request data

--- download_avatars.js
+++ download_avatars.js
@@ -6,11 +6,21 @@ var GITHUB_USER = 'n-david';
 var GITHUB_TOKEN = 'd26c217e18600e157b3df53b93e823177d6aff4d';
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var options = {
+    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+  request(options, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      var data = JSON.parse(body);
+      console.log(data);
+    }
+  });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+var options = getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
